8424ec871494dc3feb9f50815fd2a1954a6a4642
Nicolas DESFORGES <froxik77430poutlook.fr>
2023-12-14 12:59:46 +0100

    first commit

---
+++ config/database.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+let mysql = require('mysql');
+const { connect } = require('../routes/products');
+const { connect } = require('../routes/users');
+
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+
+const db = mysql.createPool({
+    host: dbHost,
+    user: dbUser,
+    password: dbPass,
+    database: dbName,
+    port: dbPort,
+    max:20,
+})
+
+
+db.getConnection((err, connexion) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Nous sommes bien connectés à la BDD");
+    }
+})
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const productsRouter = require('./routes/products');
+const usersRouter = require('./routes/users');
+const app = express();
+var cors = require('cors');
+
