9f75d2891cecf82c3f5380bb3dff79af19c647d1
Nicolas DESFORGES <froxik77430poutlook.fr>
2023-12-14 16:20:22 +0100

    product get by id

--- routes/products.js
+++ routes/products.js
@@ -15,6 +15,24 @@ router.get('/', (req, res) => {
     } )
 })
 
+router.get('/:id', (req, res) => {
+    const productId = req.params.id;
+    const sql = `SELECT * FROM products WHERE id = ${productId}`;
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.log(`Erreur lors de l'affichage du produit avec l'ID ${productId}`);
+            res.status(500).json({ message: err });
+        } else {
+            if (results.length === 0) {
+                res.status(404).json({ message: `Produit avec l'ID ${productId} non trouvé` });
+            } else {
+                res.status(200).json(results[0]);
+            }
+        }
+    });
+});
+
 
 /*router.post('/add', (req,res) => {
     const { imdbID, title, year, poster } = req.body;
